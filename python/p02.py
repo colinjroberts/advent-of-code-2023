82d6daa906fe75cc98c312516f036f05de07208f
CJ <commits@colinjroberts.com>
2023-12-02 18:18:27 -0800

    Add comments/edits to day 2

--- python/p02.py
+++ python/p02.py
@@ -9,48 +9,64 @@ def part1(input_list):
         'blue': 14,
     }
     
+    # Let's try to just brute force this one.
     result = 0
     for item in input_list:
+        
+        # Split out the first part of the string with the game number
+        # and a list of all of the rounds
         split_input = item.split(':')
         game_number = int(split_input[0][5:])
         rounds = split_input[1].strip().split('; ')
         
-        print(game_number)
+        # Iterate over the rounds and make a note of
+        # whether or not this game should be counted.
+        # A game should be counted if the counts of all
+        # colors are below the thresholds set above.
         count = True
         for round in rounds:
             values = round.split(', ')
             for value in values:
-                print(value)
                 n, color = value.split(' ')
                 if thresholds[color] < int(n):
                     count = False
+    
+        # From the problem statement, we want to sum all 
+        # games whose counts are within the thresholds
         if count:
             result += game_number
     return result
 
-
+# More splitting and brute forcing!
 def part2(input_list):
     result = 0
     for item in input_list:
+        # Again, get the rounds. We don't care about game
+        # number this time
         split_input = item.split(':')
         rounds = split_input[1].strip().split('; ')
         
-        max_cubes = {
+        # 0 initialize dict of colors
+        min_cubes = {
         'red': 0,
         'green': 0,
         'blue': 0,
         }
         
-        count = True
+        # iterate over the rounds tracking the max
+        # counts of each color
         for round in rounds:
             values = round.split(', ')
             for value in values:
                 n, color = value.split(' ')
-                if int(n) > max_cubes[color]:
-                    max_cubes[color] = int(n)
+                if int(n) > min_cubes[color]:
+                    min_cubes[color] = int(n)
         
+        # This time, the problem wants the sum of the power
+        # of each game, where power is the product of min
+        # number of cubes in each game
         power = 1
-        for color, count in max_cubes.items():
+        for color, count in min_cubes.items():
            power *= count
         result += power
     return result
